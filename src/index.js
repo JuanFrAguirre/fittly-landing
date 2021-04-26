972cb6ddbdab4b452a001b1c6a1b75de020f51e3
Juan Francisco Aguirre <juan.aguirre@naranja.com>
2021-04-26 18:09:53 -0300

    added woff font support

--- src/index.js
+++ src/index.js
@@ -4,14 +4,19 @@ import App from './App';
 import './assets/styles/styles.scss';
 import './fonts/BrandonText-Regular.otf';
 import './fonts/BrandonText-Regular.ttf';
+import './fonts/BrandonText-Regular.woff';
 import './fonts/BrandonText-Thin.otf';
 import './fonts/BrandonText-Thin.ttf';
+import './fonts/BrandonText-Thin.woff';
 import './fonts/BrandonText-Bold.otf';
 import './fonts/BrandonText-Bold.ttf';
+import './fonts/BrandonText-Bold.woff';
 import './fonts/BrandonText-Light.otf';
 import './fonts/BrandonText-Light.ttf';
+import './fonts/BrandonText-Light.woff';
 import './fonts/BrandonText-RegularItalic.otf';
 import './fonts/BrandonText-RegularItalic.ttf';
+import './fonts/BrandonText-RegularItalic.woff';
 
 ReactDOM.render(
 	<React.StrictMode>
