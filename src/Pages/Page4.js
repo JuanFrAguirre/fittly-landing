dac8a40969b92ef27a759489172f374d5ccc1321
Octavio Pareja <octaviopareja05@gmail.com>
2021-04-27 11:43:21 -0300

    page4 ready
    .

--- src/Pages/Page4.js
+++ src/Pages/Page4.js
@@ -1,64 +1,137 @@
-import icon1 from '../assets/img/icons_header/05.svg';
-import icon2 from '../assets/img/icons_header/03.svg';
-import icon3 from '../assets/img/icons_header/01.svg';
-import icon4 from '../assets/img/icons_header/06.svg';
-import icon5 from '../assets/img/icons_header/07.svg';
-import icon6 from '../assets/img/icons_header/04.svg';
-import { Card, Col, Container, Row } from 'react-bootstrap';
+import icon1 from "../assets/img/icons_header/05.svg";
+import icon2 from "../assets/img/icons_header/03.svg";
+import icon3 from "../assets/img/icons_header/01.svg";
+import icon4 from "../assets/img/icons_header/06.svg";
+import icon5 from "../assets/img/icons_header/07.svg";
+import icon6 from "../assets/img/icons_header/04.svg";
+import {
+  Card,
+  Col,
+  Container,
+  Row,
+  InputGroup,
+  FormControl,
+  Button,
+} from "react-bootstrap";
+
+import check from "../assets/img/check-circle.svg";
 
 const Page4 = () => {
-	const items = [
-		{
-			id: 1,
-			icon: icon1,
-			title: 'Promote yourself online',
-		},
-		{
-			id: 2,
-			icon: icon2,
-			title: 'Find meaningful work',
-		},
-		{
-			id: 3,
-			icon: icon3,
-			title: 'Upgrade your career',
-		},
-		{
-			id: 4,
-			icon: icon4,
-			title: 'Network outside of fittly',
-		},
-		{
-			id: 5,
-			icon: icon5,
-			title: 'Sell your services',
-		},
-		{
-			id: 6,
-			icon: icon6,
-			title: 'Meet other professionals',
-		},
-	];
+  const items = [
+    {
+      id: 1,
+      icon: icon1,
+      title: "Promote yourself online",
+    },
+    {
+      id: 2,
+      icon: icon2,
+      title: "Find meaningful work",
+    },
+    {
+      id: 3,
+      icon: icon3,
+      title: "Upgrade your career",
+    },
+    {
+      id: 4,
+      icon: icon4,
+      title: "Network outside of fittly",
+    },
+    {
+      id: 5,
+      icon: icon5,
+      title: "Sell your services",
+    },
+    {
+      id: 6,
+      icon: icon6,
+      title: "Meet other professionals",
+    },
+  ];
 
-	return (
-		<Container>
-			<Row className='step4'>
-				<Col>
-					<h2>Opportunities for growth</h2>
-					<Row className='step4__item--container'>
-						{items.map((item) => (
-							<Col md={6} lg={4} key={item.id} className='step4__item--wrapper'>
-								<Card className='step4__item'>
-									<h4 className='step4__item--title'>{item.title}</h4>
-									<img className='step4__item--img' src={item.icon} width='42px' height='32px' alt='Card icon' />
-								</Card>
-							</Col>
-						))}
-					</Row>
-				</Col>
-			</Row>
-		</Container>
-	);
+  return (
+    <Container fluid className="mainContainerStep4">
+      <Row className="orangeBG">
+        <Container>
+          <Row className="step4">
+            <Col>
+              <h2 className="text-center mb-5">Opportunities for growth</h2>
+              <Row className="step4__item--container">
+                {items.map((item) => (
+                  <Col
+                    xs={6}
+                    md={6}
+                    lg={4}
+                    key={item.id}
+                    className="step4__item--wrapper"
+                  >
+                    <Card className="step4__item">
+                      <h4 className="step4__item--title">{item.title}</h4>
+                      <img
+                        className="step4__item--img"
+                        src={item.icon}
+                        width="42px"
+                        height="32px"
+                        alt="Card icon"
+                      />
+                    </Card>
+                  </Col>
+                ))}
+              </Row>
+            </Col>
+          </Row>
+          <Row className="formNewsletter">
+            <Col>
+              <Row>
+                <Col md={12}>
+                  <h3>
+                    Sign up today to find the community you need to help your
+                    business grow!
+                  </h3>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={6} xs={12} className="mx-auto containerForm">
+                  <div className="footer__subscription--input text-center">
+                    <input type="text" placeholder="Your work email address" />
+                    <button>Join the whitelist</button>
+                  </div>
+                </Col>
+              </Row>
+              <Row>
+                <Col md={10} xs={8} className="mx-auto">
+                  <Row className="optionsCheck">
+                    <Col md={4} xs={12}>
+                      <p>
+                        <img src={check} alt="Fittly" />
+                        <br className="d-block d-sm-none" /> Access to quality
+                        job posts
+                      </p>
+                    </Col>
+                    <Col md={4} xs={12}>
+                      <p>
+                        <img src={check} alt="Fittly" />
+                        <br className="d-block d-sm-none" />
+                        Public Profile to Promote your skills
+                      </p>
+                    </Col>
+                    <Col md={4} xs={12}>
+                      <p>
+                        <img src={check} alt="Fittly" />
+                        <br className="d-block d-sm-none" /> Tons of networking
+                        opportunities
+                      </p>
+                    </Col>
+                  </Row>
+                </Col>
+              </Row>
+            </Col>
+          </Row>
+        </Container>
+      </Row>
+    </Container>
+  );
 };
 
 export default Page4;
