61ba97d22117e3c461e4c25ef3b02c313603c939
Octavio Pareja <octaviopareja05@gmail.com>
2021-04-27 14:14:30 -0300

    99%
    .

--- src/Pages/Page3.js
+++ src/Pages/Page3.js
@@ -1,51 +1,51 @@
-import { Col, Container, Media, Row } from 'react-bootstrap';
+import { Col, Container, Media, Row } from "react-bootstrap";
 
 const Page3 = () => {
-	const messages = [
-		{
-			id: 1,
-			text: 'Become a successful entrepeneur',
-		},
-		{
-			id: 1,
-			text: 'Show off your Proficient trainer skills',
-		},
-		{
-			id: 1,
-			text: 'Make enough money for your cost of living',
-		},
-		{
-			id: 1,
-			text: 'Develop a strong social media presence',
-		},
-	];
+  const messages = [
+    {
+      id: 1,
+      text: "Become a successful entrepeneur",
+    },
+    {
+      id: 1,
+      text: "Show off your Proficient trainer skills",
+    },
+    {
+      id: 1,
+      text: "Make enough money for your cost of living",
+    },
+    {
+      id: 1,
+      text: "Develop a strong social media presence",
+    },
+  ];
 
-	return (
-		<>
-			<Container>
-				<Row className='step3'>
-					<Col md={5} className='step3__img'>
-						{/* <Media>
+  return (
+    <Container fluid className="bgStep3">
+      <Container>
+        <Row className="step3">
+          <Col md={5} className="step3__img">
+            {/* <Media>
 							<img src='../assets/img/header_desktop.png' alt='Girl jumping' />
 						</Media> */}
-						<div className='step3__img--img' />
-					</Col>
-					<Col md={7} className='step3__text--container'>
-						<h2>You too can advance your fitness career</h2>
-						<Row className='step3__text'>
-							{messages.map((message) => (
-								<Col md={6}>
-									<div key={message.id} className='step3__text--item'>
-										<p>{message.text}</p>
-									</div>
-								</Col>
-							))}
-						</Row>
-					</Col>
-				</Row>
-			</Container>
-		</>
-	);
+            <div className="step3__img--img" />
+          </Col>
+          <Col md={7} className="step3__text--container">
+            <h2>You too can advance your fitness career</h2>
+            <Row className="step3__text">
+              {messages.map((message) => (
+                <Col md={6}>
+                  <div key={message.id} className="step3__text--item">
+                    <p>{message.text}</p>
+                  </div>
+                </Col>
+              ))}
+            </Row>
+          </Col>
+        </Row>
+      </Container>
+    </Container>
+  );
 };
 
 export default Page3;
--- src/Pages/Page7.js
+++ src/Pages/Page7.js
@@ -1,39 +1,82 @@
-import { Col, Container, Row } from 'react-bootstrap';
+import { Col, Container, Row } from "react-bootstrap";
+import logo from "../assets/img/logo_header.svg";
+
+import ig from "../assets/img/socialmedia/ig.svg";
+import tw from "../assets/img/socialmedia/tw.svg";
+import yt from "../assets/img/socialmedia/yt.svg";
 
 const Page7 = () => {
-	return (
-		<>
-			<Container fluid className='footer--container'>
-				<Container className='footer'>
-					<Row className='footer__subscription'>
-						<Col className='footer__subscription--text' md={12} lg={8}>
-							<h4>
-								Sign up today for exclusive access to our premium subscription for your <span>first 3 months</span>
-							</h4>
-							<div className='footer__subscription--input'>
-								<input type='text' placeholder='Your work email address' />
-								<button>Join the whitelist</button>
-							</div>
-						</Col>
-						<Col className='footer__subscription--img' md={0} lg={4}></Col>
-					</Row>
-					<Row className='footer__info'>
-						<Col className='footer__info--section'>
-							<div className='' />
-						</Col>
-						<Col className='footer__info--section'>
-							<p>C 2021 Fittly</p>
-						</Col>
-						<Col className='footer__info--section footer__info--links'>
-							<div>IG</div>
-							<div>TW</div>
-							<div>YT</div>
-						</Col>
-					</Row>
-				</Container>
-			</Container>
-		</>
-	);
+  return (
+    <>
+      <Container fluid className="footer--container">
+        <Container className="footer">
+          <Row className="footer__subscription">
+            <Col className="footer__subscription--text" md={12} lg={8}>
+              <h4>
+                Sign up today for exclusive access to our premium subscription
+                for your <span>first 3 months</span>
+              </h4>
+              <div className="footer__subscription--input">
+                <input type="text" placeholder="Your work email address" />
+                <button>Join the whitelist</button>
+              </div>
+            </Col>
+            <Col className="footer__subscription--img" md={0} lg={4}></Col>
+          </Row>
+          <Row className="footer_bar">
+            <Col
+              md={3}
+              xs={12}
+              className="text-sm-left text-center order-0 order-sm-0"
+            >
+              <img src={logo} alt="Fittly" className="logo" />
+            </Col>
+            <Col md={6} xs={12} className="text-center order-2 order-sm-1">
+              <p className="copyright">© 2021 Fittly</p>
+            </Col>
+            <Col
+              md={3}
+              xs={12}
+              className="text-sm-right text-center pt-2 order-1 order-sm-2"
+            >
+              <ul className="list-inline list-unstyled">
+                <li className="list-inline-item p-1">
+                  <a
+                    href="https://instagram.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social_media_link"
+                  >
+                    <img src={ig} style={{ height: "10px" }} alt="Instagram" />
+                  </a>
+                </li>
+                <li className="list-inline-item p-1">
+                  <a
+                    href="https://twitter.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social_media_link"
+                  >
+                    <img src={tw} alt="Twitter" />
+                  </a>
+                </li>
+                <li className="list-inline-item p-1">
+                  <a
+                    href="https://yotube.com"
+                    target="_blank"
+                    rel="noreferrer"
+                    className="social_media_link"
+                  >
+                    <img src={yt} alt="YouTube" />
+                  </a>
+                </li>
+              </ul>
+            </Col>
+          </Row>
+        </Container>
+      </Container>
+    </>
+  );
 };
 
 export default Page7;
